b41249b13a87d9af2171bfc8a84cd92b66cc56ad
Mcjunges <mcjunges@gmail.com>
2024-01-04 17:14:39 -0300

    Criação da estrutura

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>Home - Gold Lions</title>
+</head>
+
+<?php
+include "html/header.html";
+?>
+
+<body>
+    <h1>Olá, mundo!</h1>
+    <h2>Início do site do Gold Lions</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptate pariatur harum maxime odit consectetur distinctio minima maiores dignissimos deleniti impedit quisquam, illo perspiciatis et possimus libero quis accusamus neque.</p>
+
+    <?php
+    include "html/footer.html";
+    ?>
+</body>
+
+</html>
